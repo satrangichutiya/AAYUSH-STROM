70686abb5509f22862559147ad087e59e966ffe7
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-03-04 19:57:52 +0530

    ꜱᴛᴏʀᴍ 3.0

---
+++ main.py
@@ -0,0 +1,50 @@
+import sys
+import glob
+import asyncio
+import logging
+import importlib
+import urllib3
+from pathlib import Path
+from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
+
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def load_plugins(plugin_name):
+    path = Path(f"STORM/modules/{plugin_name}.py")
+    spec = importlib.util.spec_from_file_location(f"STORM.modules.{plugin_name}", path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["STORM.modules." + plugin_name] = load
+    print("ꜱᴛᴏʀᴍ ʜᴀꜱ ɪᴍᴘᴏʀᴛᴇᴅ" + plugin_name)
+
+
+files = glob.glob("STORM/modules/*.py")
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+
+print("ꜱᴛᴏʀᴍ ɪꜱ ᴅᴇᴘʟᴏʏᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ 🫧")
+
+
+async def main():
+    await X1.run_until_disconnected()
+    await X2.run_until_disconnected()
+    await X3.run_until_disconnected()
+    await X4.run_until_disconnected()
+    await X5.run_until_disconnected()
+    await X6.run_until_disconnected()
+    await X7.run_until_disconnected()
+    await X8.run_until_disconnected()
+    await X9.run_until_disconnected()
+    await X10.run_until_disconnected()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
