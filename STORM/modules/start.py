76f5a0750766b9930d9a14e422d94b57686066a9
ᴅᴇᴠɪʟ <157782315+kxunal1834@users.noreply.github.com>
2024-04-13 10:11:41 +0530

    Update start.py

--- STORM/modules/start.py
+++ STORM/modules/start.py
@@ -1,41 +1,19 @@
-#MIT License
-
-#Copyright (c) 2024 ᴋᴜɴᴀʟ [AFK]
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
-
 from telethon import __version__, events, Button
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_OP = [
     [
-        Button.url("ꜱᴇɴꜱᴇɪ 🥀", "https://t.me/Kexx_XD"),
-        Button.url("ꜱᴜᴘᴘᴏʀᴛ ✨", "https://t.me/STORM_CHATZ"),
+        Button.url("ꜱᴇɴꜱᴇɪ 🥀", "https://t.me/HOD_AYUSH_PYROGRAM_CLIENT"),
+        Button.url("ꜱᴜᴘᴘᴏʀᴛ ✨", "https://t.me/HOD_AYUSH_PYROGRAM_CLIENT"),
     ],
     [
         Button.url(
-            "ɢʀᴏᴜᴘ 🧸", "https://t.me/FriendCastel"
+            "ɢʀᴏᴜᴘ 🧸", "https://t.me/HOD_AYUSH_PYROGRAM_CLIENT"
         ),
     ],
     [
         Button.url("ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ❄️", "https://github.com/VARC9210/STORM"),
-        Button.url("ᴄʜᴀɴɴᴇʟ ☁️", "https://t.me/STORM_TECHH"),
+        Button.url("ᴄʜᴀɴɴᴇʟ ☁️", "https://t.me/HOD_AYUSH_PYROGRAM_CLIENT"),
     ],
 ]
 
@@ -55,13 +33,13 @@ async def start(event):
         bot_name = KEX.first_name
         bot_id = KEX.id
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}]\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n➖➖➖➖➖➖➖➖➖➖➖\n"
-        TEXT += f"» **ꜱᴇɴꜱᴇɪ : [⏤͟͞〲ᴋᴇx](https://t.me/kexx_xd)**\n"
-        TEXT += f"» **ꜱᴛᴏʀᴍ :** `M3.0` \n"
+        TEXT += f"» **ꜱᴇɴꜱᴇɪ : [ꜱᴇɴꜱᴇɪ](https://t.me/HOD_AYUSH_PYROGRAM_CLIENT)**\n"
+        TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ :** `M1.0` \n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ :** `3.11` \n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ :** `{__version__}`\n➖➖➖➖➖➖➖➖➖➖➖"        
         await event.client.send_file(
                     event.chat_id,  
-                    "https://graph.org/file/7f4da811955cc9c3c763f.jpg",
+                    "https://graph.org/file/fb1ae9fa7f75d73efb5f2.jpg",
                     caption=TEXT, 
                     buttons=START_OP
                 )
