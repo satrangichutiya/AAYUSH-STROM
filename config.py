6e961eb0c0ea2dc2db79497bf75dbfd5cccf2331
ᴅᴇᴠɪʟ <157782315+kxunal1834@users.noreply.github.com>
2024-04-13 09:48:21 +0530

    ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ

--- config.py
+++ config.py
@@ -1,25 +1,3 @@
-#MIT License
-
-#Copyright (c) 2024 ᴋᴜɴᴀʟ [AFK]
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
-
 import logging
 from telethon import TelegramClient
 from os import getenv
@@ -44,22 +22,22 @@ BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="6257927828").split()))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="7021594661").split()))
 for x in DEV:
     SUDO_USERS.append(x)
-OWNER_ID = int(getenv("OWNER_ID", default="6257927828"))
+OWNER_ID = int(getenv("OWNER_ID", default="7021594661"))
 SUDO_USERS.append(OWNER_ID)
 
 
 # ------------- CLIENTS -------------
 
-X1 = TelegramClient('ꜱ ᴛ ᴏ ʀ ᴍ 1', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-X2 = TelegramClient('ꜱ ᴛ ᴏ ʀ ᴍ 2', API_ID, API_HASH).start(bot_token=BOT_TOKEN2)
-X3 = TelegramClient('ꜱ ᴛ ᴏ ʀ ᴍ 3', API_ID, API_HASH).start(bot_token=BOT_TOKEN3)
-X4 = TelegramClient('ꜱ ᴛ ᴏ ʀ ᴍ 4', API_ID, API_HASH).start(bot_token=BOT_TOKEN4)
-X5 = TelegramClient('ꜱ ᴛ ᴏ ʀ ᴍ 5', API_ID, API_HASH).start(bot_token=BOT_TOKEN5)
-X6 = TelegramClient('ꜱ ᴛ ᴏ ʀ ᴍ 6', API_ID, API_HASH).start(bot_token=BOT_TOKEN6)
-X7 = TelegramClient('ꜱ ᴛ ᴏ ʀ ᴍ 7', API_ID, API_HASH).start(bot_token=BOT_TOKEN7)
-X8 = TelegramClient('ꜱ ᴛ ᴏ ʀ ᴍ 8', API_ID, API_HASH).start(bot_token=BOT_TOKEN8)
-X9 = TelegramClient('ꜱ ᴛ ᴏ ʀ ᴍ 9', API_ID, API_HASH).start(bot_token=BOT_TOKEN9)
-X10 = TelegramClient('ꜱ ᴛ ᴏ ʀ ᴍ 10', API_ID, API_HASH).start(bot_token=BOT_TOKEN10)
+X1 = TelegramClient('ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ 1', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+X2 = TelegramClient('ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ 2', API_ID, API_HASH).start(bot_token=BOT_TOKEN2)
+X3 = TelegramClient('ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ 3', API_ID, API_HASH).start(bot_token=BOT_TOKEN3)
+X4 = TelegramClient('ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ 4', API_ID, API_HASH).start(bot_token=BOT_TOKEN4)
+X5 = TelegramClient('ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ 5', API_ID, API_HASH).start(bot_token=BOT_TOKEN5)
+X6 = TelegramClient('ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ 6', API_ID, API_HASH).start(bot_token=BOT_TOKEN6)
+X7 = TelegramClient('ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ 7', API_ID, API_HASH).start(bot_token=BOT_TOKEN7)
+X8 = TelegramClient('ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ 8', API_ID, API_HASH).start(bot_token=BOT_TOKEN8)
+X9 = TelegramClient('ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ 9', API_ID, API_HASH).start(bot_token=BOT_TOKEN9)
+X10 = TelegramClient('ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ 10', API_ID, API_HASH).start(bot_token=BOT_TOKEN10)
