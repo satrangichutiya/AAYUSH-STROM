cea4c5c8e924bdcf37f383644f14cac60afb86f9
ᴅᴇᴠɪʟ <157782315+kxunal1834@users.noreply.github.com>
2024-04-13 14:46:08 +0530

    Update start.py

--- STORM/modules/start.py
+++ STORM/modules/start.py
@@ -3,7 +3,7 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_OP = [
     [
-        Button.url("ꜱᴇɴꜱᴇɪ 🥀", "https://t.me/HOD_AYUSH_PYROGRAM_CLIENT"),
+        Button.url("ꜱᴇɴꜱᴇɪ 🥀", "https://t.me/GOD_AYUSH_PYROGRAM_CLIENT"),
     ],
     [
         Button.url("ꜱᴜᴘᴘᴏʀᴛ ✨", "https://t.me/PYROGRAMXSUPPORT"),
@@ -27,7 +27,7 @@ async def start(event):
         bot_name = KEX.first_name
         bot_id = KEX.id
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}]\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n➖➖➖➖➖➖➖➖➖➖➖\n"
-        TEXT += f"» **ꜱᴇɴꜱᴇɪ : [ꜱᴇɴꜱᴇɪ](https://t.me/HOD_AYUSH_PYROGRAM_CLIENT)**\n"
+        TEXT += f"» **ꜱᴇɴꜱᴇɪ : [ꜱᴇɴꜱᴇɪ](https://t.me/GOD_AYUSH_PYROGRAM_CLIENT)**\n"
         TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ :** `M1.0` \n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ :** `3.11` \n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ :** `{__version__}`\n➖➖➖➖➖➖➖➖➖➖➖"        
