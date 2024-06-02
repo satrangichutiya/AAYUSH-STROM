dc3e6c0378ee73710b263cb86b77a3f6cb98adc5
devgramop <168399896+devgramop@users.noreply.github.com>
2024-06-02 13:04:53 +0530

    Update start.py

--- STORM/modules/start.py
+++ STORM/modules/start.py
@@ -3,11 +3,11 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 
 START_OP = [
     [
-        Button.url("ꜱᴇɴꜱᴇɪ 🥀", "https://t.me/GOD_AYUSH_PYROGRAM_CLIENT"),
+        Button.url("ꜱᴇɴꜱᴇɪ 🥀", "@GOD_AYUSH_PYROGRAM_V1"),
     ],
     [
-        Button.url("ꜱᴜᴘᴘᴏʀᴛ ✨", "https://t.me/PYROGRAMXSUPPORT"),
-        Button.url("ᴄʜᴀɴɴᴇʟ ☁️", "https://t.me/LABDESHXSUPPORT"),
+        Button.url("ꜱᴜᴘᴘᴏʀᴛ ✨", "https://t.me/AYUSHXROBOT_HUB"),
+        Button.url("ᴄʜᴀɴɴᴇʟ ☁️", "https://t.me/AYUSHXBOTS_HUB"),
     ],
 ]
 
@@ -27,7 +27,7 @@ async def start(event):
         bot_name = KEX.first_name
         bot_id = KEX.id
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}]\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n➖➖➖➖➖➖➖➖➖➖➖\n"
-        TEXT += f"» **ꜱᴇɴꜱᴇɪ : [ꜱᴇɴꜱᴇɪ](https://t.me/GOD_AYUSH_PYROGRAM_CLIENT)**\n"
+        TEXT += f"» **ꜱᴇɴꜱᴇɪ : [ꜱᴇɴꜱᴇɪ](https://t.me/@GOD_AYUSH_PYROGRAM_V1)**\n"
         TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ x ꜱᴘᴀᴍ :** `M1.0` \n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ :** `3.11` \n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ :** `{__version__}`\n➖➖➖➖➖➖➖➖➖➖➖"        
