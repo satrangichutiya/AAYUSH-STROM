6d6c4d00a1ea8d42ccf77d82968cc1c7a36bcec5
devgramop <168399896+devgramop@users.noreply.github.com>
2024-06-09 13:22:25 +0530

    Create start.py

---
+++ STORM/modules/start.py
@@ -0,0 +1,133 @@
+#MIT License
+
+
+
+#Copyright (c) 2024 ᴋᴜɴᴀʟ [AFK]
+
+
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+
+#of this software and associated documentation files (the "Software"), to deal
+
+#in the Software without restriction, including without limitation the rights
+
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+
+#copies of the Software, and to permit persons to whom the Software is
+
+#furnished to do so, subject to the following conditions:
+
+
+
+#The above copyright notice and this permission notice shall be included in all
+
+#copies or substantial portions of the Software.
+
+
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+
+#SOFTWARE.
+
+
+
+from telethon import version, events, Button
+
+from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
+
+
+
+START_OP = [
+
+    [
+
+        Button.url("ꜱᴇɴꜱᴇɪ 🥀", "https://t.me/ll_DRAGON_XD_ll"),
+
+        Button.url("ꜱᴜᴘᴘᴏʀᴛ ✨", "https://t.me/ll_DRAGON_XD_SUPPORT_ll"),
+
+    ],
+
+    [
+
+        Button.url(
+
+            "ɢʀᴏᴜᴘ 🧸", "https://t.me/ll_DRAGON_XD_SUPPORT_ll"
+
+        ),
+
+    ],
+
+    [
+
+        Button.url("ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ❄️", "https://github.com/devgramop/AAYUSH-STROM"),
+
+        Button.url("ᴄʜᴀɴɴᴇʟ ☁️", "https://t.me/ll_DRAGON_XD_SUPPORT_ll"),
+
+    ],
+
+]
+
+
+
+@X1.on(events.NewMessage(pattern="/start"))
+
+@X2.on(events.NewMessage(pattern="/start"))
+
+@X3.on(events.NewMessage(pattern="/start"))
+
+@X4.on(events.NewMessage(pattern="/start"))
+
+@X5.on(events.NewMessage(pattern="/start"))
+
+@X6.on(events.NewMessage(pattern="/start"))
+
+@X7.on(events.NewMessage(pattern="/start"))
+
+@X8.on(events.NewMessage(pattern="/start"))
+
+@X9.on(events.NewMessage(pattern="/start"))
+
+@X10.on(events.NewMessage(pattern="/start"))
+
+async def start(event):
+
+    if event.is_private:
+
+        KEX = await event.client.get_me()
+
+        bot_name = KEX.first_name
+
+        bot_id = KEX.id
+
+        TEXT = f"ʜᴇʏ [{event.sender.first_name}]\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})\n➖➖➖➖➖➖➖➖➖➖➖\n"
+
+        TEXT += f"» ꜱᴇɴꜱᴇɪ : [⏤‌〲ᴋᴇx](https://t.me/ll_DRAGON_XD_ll)\n"
+
+        TEXT += f"» ꜱᴛᴏʀᴍ : M3.0 \n"
+
+        TEXT += f"» ᴘʏᴛʜᴏɴ : 3.11 \n"
+
+        TEXT += f"» ᴛᴇʟᴇᴛʜᴏɴ : {version}\n➖➖➖➖➖➖➖➖➖➖➖"        
+
+        await event.client.send_file(
+
+                    event.chat_id,  
+
+                    "https://telegra.ph/file/20e905f22c14c40d9bba7.jpg",
+
+                    caption=TEXT, 
+
+                    buttons=START_OP
+
+      )
